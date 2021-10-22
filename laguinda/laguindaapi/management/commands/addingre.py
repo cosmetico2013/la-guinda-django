6ab11a45e4a0e94a63e861f04582937fa1e175a8
cosmetico2013 <serlagee44@gmail.com>
2021-10-22 21:37:55 +0200

    p4

--- laguinda/laguindaapi/management/commands/addingre.py
+++ laguinda/laguindaapi/management/commands/addingre.py
@@ -1,13 +1,13 @@
 from django.core.management.base import BaseCommand, CommandError
 from laguindaapi.models import Ingrediente
 
-class command(BaseCommand):
+class Command(BaseCommand):
     help = 'Añade un nuevo ingrediente'
 
     def add_arguments(selft, parse):
-        parse.add_argument('nom', help="nombre del igrediente")
+        parse.add_argument('nombre', help="nombre del igrediente")
     
     def handle(self, *args, **options):
-        name=options["nom"]
-        Ingrediente.objects.create(nom=name)
+        name=options["nombre"]
+        Ingrediente.objects.create(nombre=name)
         self.stdout.write('Ingrediente añadido')
