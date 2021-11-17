44bd20fc8fdf6c106b0c0d9a544953c19a551a5c
cosmetico2013 <serlagee44@gmail.com>
2021-11-17 19:39:23 +0100

    p6

--- laguinda/laguindaapi/models.py
+++ laguinda/laguindaapi/models.py
@@ -20,7 +20,7 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=50)
     precio = models.DecimalField(max_digits=5, decimal_places=2, default=0)
     oferta = models.ForeignKey('Oferta', on_delete=models.CASCADE)
-    imagen = models.FileField(upload_to="imagenes/")
+    imagen = models.FileField(upload_to="img/")
 
     def __str__(self):
         return self.nombre
